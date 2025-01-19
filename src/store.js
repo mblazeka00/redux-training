acde9732670ed45f0c21d778e6c0b14389e33f86
Marko Blažeka <marko.blazeka00@gmail.com>
2025-01-19 21:20:07 +0100

    Created redux store and Action creators

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -18,8 +20,12 @@ function reducer(state = initialState, action) {
       };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      // Later
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -32,3 +38,45 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+/*
+store.dispatch({ type: "account/deposit", payload: 1500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy car" },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
+*/
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(1500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, "car"));
+store.dispatch(payLoan());
+
+console.log(store.getState());
