6d2dbedc4c327abed76a5927f7ac8a67c04089d5
Marko Blažeka <marko.blazeka00@gmail.com>
2025-01-20 11:41:21 +0100

    Created store slices and features folder

--- src/store.js
+++ src/store.js
@@ -1,82 +1,12 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-      };
-    case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-/*
-store.dispatch({ type: "account/deposit", payload: 1500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
-
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy car" },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
 
-console.log(store.getState());
+const store = createStore(rootReducer);
 
-store.dispatch({ type: "account/payLoan" });
-console.log(store.getState());
-*/
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(1500));
-store.dispatch(withdraw(200));
-store.dispatch(requestLoan(1000, "car"));
-store.dispatch(payLoan());
-
-console.log(store.getState());
+export default store;
